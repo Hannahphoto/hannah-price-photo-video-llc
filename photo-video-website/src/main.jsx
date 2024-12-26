f1bf017406e5b40b966306cd35818ef988620ec7
Hannahphoto <hannahprice67@gmail.com>
2024-12-26 14:11:43 -0500

    pages and components page

--- photo-video-website/src/main.jsx
+++ photo-video-website/src/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@fontsource/red-hat-mono'; // Defaults to 400 weight
+import 'foundation-sites/dist/css/foundation.min.css'; //foundation framework
+import { Grid, Cell,Menu,MenuItem } from "react-foundation";
 import App from './App.jsx'
+
+//bring in the pages so the router will show appropriate view/page
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
