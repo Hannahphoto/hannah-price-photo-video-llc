c9f626a090943516562bc3ed31e64c79727656b9
Hannahphoto <hannahprice67@gmail.com>
2024-12-31 16:25:59 -0500

    page appearing but font still not working

--- photo-video-website/src/main.jsx
+++ photo-video-website/src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from 'react-dom/client'
 import './index.css'
 import '@fontsource/red-hat-mono'; // Defaults to 400 weight
 import 'foundation-sites/dist/css/foundation.min.css'; //foundation framework
-// import { Grid, Cell,Menu,MenuItem } from "react-foundation";
+import { Grid, Cell,Menu,MenuItem } from "react-foundation";
 import App from './App.jsx'
 
 //bring in the pages so the router will show appropriate view/page
